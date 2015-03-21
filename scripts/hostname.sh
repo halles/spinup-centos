76c285df5b7a6fb61cee994ce12ad886efc6a01b
Matías Halles <matias.halles@wikot.com>
2015-03-21 20:58:56 -0300

    UPDATE: Cambiar la manera en la que fija la IP local al hostname

--- scripts/hostname.sh
+++ scripts/hostname.sh
@@ -3,8 +3,7 @@ server_hostname=${args[0]}
 cluster_hostname=${args[1]}
 
 echo 'HOSTNAME="$server_hostname.$cluster_hostname"' >> /etc/sysconfig/network
-server_ip=`ifconfig eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
-echo -e "$server_ip \t $server_hostname.$cluster_hostname $server_hostname" >> /etc/hosts
+echo -e "127.0.0.1 \t $server_hostname.$cluster_hostname $server_hostname" >> /etc/hosts
 hostname $server_hostname.$cluster_hostname
 systemctl restart network.service
 
