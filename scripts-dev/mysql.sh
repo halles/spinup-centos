c683b777d5dcbfdcad28ee3c9bfa675a4f31972d
Matías Halles <matias.halles@wikot.com>
2015-03-23 18:32:56 -0300

    UPDATE: Archivo de configuración custom para mysqld

--- scripts-dev/mysql.sh
+++ scripts-dev/mysql.sh
@@ -10,3 +10,9 @@ yum -y install http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
 yum -y install mysql mysql-server
 systemctl enable mysqld.service
 systemctl stop mysqld.service
+
+# Configuración por defecto
+
+cp config-dev/my.cnf /etc/my.cnf
+#chown -R vagrant:vagrant -R /var/run/mysqld
+#chown -R vagrant:vagrant /var/lib/mysql
