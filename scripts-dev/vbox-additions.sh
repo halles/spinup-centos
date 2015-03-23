1f728d02629ebc14e6c219b75cbce2812ce85e7a
Matías Halles <matias.halles@wikot.com>
2015-03-23 00:32:24 -0300

    UPDATE: Corrección en la instalación de Virtual Box Additions

--- scripts-dev/vbox-additions.sh
+++ scripts-dev/vbox-additions.sh
@@ -1,7 +1,9 @@
+yum install -y gcc make kernel-devel
+
 mkdir VBoxLinuxAdditions
 mount /dev/cdrom VBoxLinuxAdditions
 cd VBoxLinuxAdditions
-./VBoxLinuxAdditions.run
+./VBoxLinuxAdditions.run install --force
 cd ..
 umount /dev/cdrom
 rmdir VBoxLinuxAdditions
