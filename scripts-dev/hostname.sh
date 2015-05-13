30012d59918b461a299ac4943021ee30c6d290c8
Matías Halles <matias.halles@wikot.com>
2015-05-12 22:38:53 -0300

    UPDATE: Bug en definición de nombre de hostname corregido en WCDE

--- scripts-dev/hostname.sh
+++ scripts-dev/hostname.sh
@@ -2,7 +2,7 @@ args=("$@")
 server_hostname=${args[0]}
 cluster_hostname=${args[1]}
 
-echo 'HOSTNAME="$server_hostname.$cluster_hostname"' >> /etc/sysconfig/network
+echo HOSTNAME="$server_hostname.$cluster_hostname" >> /etc/sysconfig/network
 echo -e "127.0.0.1 \t $server_hostname.$cluster_hostname $server_hostname" >> /etc/hosts
 hostname $server_hostname.$cluster_hostname
 systemctl restart network.service
