dc65140da770f12f2aa12ec912844bcfdb00fa0c
Matías Halles <matias.halles@wikot.com>
2015-07-06 15:51:26 -0300

    ruta de archivo de configuración de repositorio incorrecta

--- scripts-dev/mongodb.sh
+++ scripts-dev/mongodb.sh
@@ -2,7 +2,7 @@
 # Centos viene por defecto con una version antigua de MongoDB
 # Reempalzar repositorio con versión oficial
 
-cp config-dev/yum.res.d/mongodb-org-3.0.repo /etc/yum.res.d/mongodb-org-3.0.repo
+cp config-dev/yum.repos.d/mongodb-org-3.0.repo /etc/yum.repos.d/mongodb-org-3.0.repo
 
 sudo yum install -y mongodb-org
 
