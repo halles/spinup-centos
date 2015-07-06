90f211550c34a2dcf84ecd80af027162b3b24789
Matías Halles <matias.halles@wikot.com>
2015-07-06 15:20:38 -0300

    Builder de box para hashicorp sin tomar en cuenta versión de virtual box.

--- spinup-wcde-build.sh
+++ spinup-wcde-build.sh
@@ -1,9 +1,8 @@
 args=("$@")
 box_base_name=${args[0]}
 box_version=${args[1]}
-vb_version=${args[2]}
 
-output_file="build/wcde-centos7-vb$vb_version-$box_version.box"
+output_file="build/wcde-centos7-$box_version.box"
 
 vagrant package --base "'$box_base_name'" --output output_file
 vagrant box add halles/wcde output_file
