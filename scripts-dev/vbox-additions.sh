33a5b90294c6e79ed71d3f7c01b802e0bcf2a19e
Matías Halles <matias.halles@wikot.com>
2015-07-06 15:18:02 -0300

    se omite la instalacion de vb additions en la creación de máquina virtual para dejar la responsabilidad al momento de usar la máquina.

--- scripts-dev/vbox-additions.sh
+++ scripts-dev/vbox-additions.sh
@@ -1,9 +1,1 @@
 yum install -y gcc make kernel-devel
-
-mkdir VBoxLinuxAdditions
-mount /dev/cdrom VBoxLinuxAdditions
-cd VBoxLinuxAdditions
-./VBoxLinuxAdditions.run install --force
-cd ..
-umount /dev/cdrom
-rmdir VBoxLinuxAdditions
